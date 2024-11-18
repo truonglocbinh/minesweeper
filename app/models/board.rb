7bbf00ad374153fce9f4e67a030f09e3cce91484
truonglocbinh93 <truonglocbinh93@gmail.com>
2024-11-18 18:04:30 +0700

    add board state

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,6 +4,14 @@ class Board < ApplicationRecord
   validates :width, :height, :mines, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validate :max_mines
 
+  def generate_board_state
+    Array.new(height) do |row|
+      Array.new(width) do |col|
+        { mine: mine_positions.include?([col, row]) }
+      end
+    end
+  end
+
   private
 
   def max_mines
